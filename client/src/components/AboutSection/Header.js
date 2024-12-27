4b56fbee720f85d0a3454ead87fd2e51a17dbf9f
Rouslan Ouvarov <smfdgot@gmail.com>
2024-12-27 10:21:26 +0100

    padding from about

--- client/src/components/AboutSection/Header.js
+++ client/src/components/AboutSection/Header.js
@@ -3,23 +3,22 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="flex flex-col items-start justify-start xxl:items-center xxl:justify-center w-full ml-12 mt-48 xxl:mt-20 relative z-50">
-      <div className="bg-white/80 px-5 py-3 rounded-lg text-left mb-6 max-w-full sm:max-w-screen-sm md:max-w-screen-md">
-        <h1 className="text-xl sm:text-3xl md:text-3xl font-bold text-gray-800 overflow-hidden">
-          Welcome To My Portfolio
-        </h1>
-      </div>
-      <div className="flex flex-col items-start text-left space-y-4 xxl:items-center xxl:text-center">
-        <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold">
-          Rouslan Ouvarov
-        </h2>
-        <h3 className="text-2xl sm:text-3xl md:text-4xl ">I AM A WEB DEVELOPER</h3>
-        <h4 className="text-2xl sm:text-2xl font-medium xxl:text-center">About</h4>
-        <p className="max-w-md sm:text-2xl text-xl text-gray-300">
-          I am passionate about web development, continuously working on improving my skills. I specialize in building modern, responsive, and user-friendly web applications. My goal is to create seamless digital experiences that meet both functional and aesthetic needs.
-        </p>
-      </div>
-    </header>
+<header className="flex flex-col items-start justify-start xxl:items-center xxl:justify-center w-full px-6 sm:px-12 mt-48 xxl:mt-20 relative z-50">
+  <div className="bg-white/80 px-5 py-3 rounded-lg text-left mb-6 max-w-full sm:max-w-screen-sm md:max-w-screen-md">
+    <h1 className="text-xl sm:text-3xl md:text-3xl font-bold text-gray-800 overflow-hidden">
+      Welcome To My Portfolio
+    </h1>
+  </div>
+  <div className="flex flex-col items-start text-left space-y-4 xxl:items-center xxl:text-center">
+    <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold">Rouslan Ouvarov</h2>
+    <h3 className="text-2xl sm:text-3xl md:text-4xl">I AM A WEB DEVELOPER</h3>
+    <h4 className="text-2xl sm:text-2xl font-medium xxl:text-center">About</h4>
+    <p className="max-w-md sm:text-2xl text-xl text-gray-300">
+      I am passionate about web development, continuously working on improving my skills. I specialize in building modern, responsive, and user-friendly web applications. My goal is to create seamless digital experiences that meet both functional and aesthetic needs.
+    </p>
+  </div>
+</header>
+
   );
 }
 
