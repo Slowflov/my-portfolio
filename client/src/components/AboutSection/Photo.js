e8f555b808893c26ea3d54502c7bdb594c459e26
Rouslan Ouvarov <smfdgot@gmail.com>
2025-01-12 18:56:56 +0100

    photo locations are set up

--- client/src/components/AboutSection/Photo.js
+++ client/src/components/AboutSection/Photo.js
@@ -5,22 +5,22 @@ import Myphoto from '../../assets/Myphot.webp';
 function Photo() {
   return (
     <>
-<div className="relative top-[-800px] right-14 w-full h-screen z-0 flex justify-end items-center mb-[400px] xxl:hidden">
+<div className="relative right-14 w-full z-0 flex justify-end items-center xxl:hidden">
   <img
     src={Myphoto}
     alt="Sitting next to the Eiffel Tower in Paris, enjoying a moment of serenity in one of the most iconic cities in the world."
-    className="w-[400px] h-[400px] shadow-lg sm:w-[350px] sm:h-[350px] md:w-[340px] md:h-[350px] lg:w-[400px] lg:h-[400px]"
+    className="w-[400px] h-[400px] shadow-lg sm:w-[350px] sm:h-[350px] md:w-[370px] md:h-[400px] lg:w-[400px] lg:h-[400px] transform translate-y-[-100%]"
     style={{
       clipPath: "polygon(0% 0%, 95% 0%, 100% 100%, 9% 89%)",
       objectFit: "cover",
       objectPosition: "center",
-      transform: "scale(1.2)",
       transformOrigin: "center",
       pointerEvents: "none"
     }}
     loading="lazy"
   />
 </div>
+
 
       <div className="relative mb-14 ml-6 w-[350px] h-[350px] mt-10 z-0 flex justify-center items-end hidden xxl:block">
         <img
