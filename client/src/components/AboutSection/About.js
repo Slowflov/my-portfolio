fefac95c434bfef70d5fce2fe7a4f65058d2231d
Rouslan Ouvarov <smfdgot@gmail.com>
2025-01-12 18:43:29 +0100

    added an adapter for phones with different programs

--- client/src/components/AboutSection/About.js
+++ client/src/components/AboutSection/About.js
@@ -1,15 +1,31 @@
-// src/components/AboutSection/AboutSection.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import Navigation from './Navigation';
 import Header from './Header';
 import Photo from './Photo';
 
 function AboutSection() {
+  const [isAndroid, setIsAndroid] = useState(false);
+  const [isIphone, setIsIphone] = useState(false);
+
+  useEffect(() => {
+    // Проверяем, если это Android
+    if (/Android/i.test(navigator.userAgent)) {
+      setIsAndroid(true);
+    }
+    // Проверяем, если это iPhone
+    if (/iPhone|iPod/i.test(navigator.userAgent)) {
+      setIsIphone(true);
+    }
+  }, []);
+
   return (
-    <section className="h-screen bg-gradient-to-b from-[#264653] to-[#04192e] flex flex-col items-center justify-start text-white px-3 py-2 relative overflow-hidden">
+    <section
+      className={`bg-gradient-to-b from-[#264653] to-[#04192e] flex flex-col items-center justify-start text-white px-3 py-2 relative overflow-hidden
+      ${isAndroid ? 'min-h-[calc(100vh+50px)]' : ''} ${isIphone ? 'min-h-[calc(100vh+60px)]' : ''}`}
+    >
       <Helmet>
-        <title>About Me - My Portfolio</title> 
+        <title>About Me - My Portfolio</title>
         <meta name="description" content="Learn more about my journey as a developer and my skills." />
         <meta name="keywords" content="about me, developer, web development, portfolio" />
         <meta name="author" content="Rouslan" />
--- client/src/components/AboutSection/Photo.js
+++ client/src/components/AboutSection/Photo.js
@@ -5,7 +5,7 @@ import Myphoto from '../../assets/Myphot.webp';
 function Photo() {
   return (
     <>
-<div className="relative top-[-410px] right-14 w-full h-screen z-0 flex justify-end items-center mb-[400px] xxl:hidden">
+<div className="relative top-[-800px] right-14 w-full h-screen z-0 flex justify-end items-center mb-[400px] xxl:hidden">
   <img
     src={Myphoto}
     alt="Sitting next to the Eiffel Tower in Paris, enjoying a moment of serenity in one of the most iconic cities in the world."
@@ -21,6 +21,7 @@ function Photo() {
     loading="lazy"
   />
 </div>
+
       <div className="relative mb-14 ml-6 w-[350px] h-[350px] mt-10 z-0 flex justify-center items-end hidden xxl:block">
         <img
           srcSet={`${Myphoto}?w=300 300w, ${Myphoto}?w=600 600w, ${Myphoto}?w=900 900w`} 
